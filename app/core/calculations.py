824f28f9caca8a77fbe0ce225714b08d286b94a3
shobhit <shobhitsarkar02@gmail.com>
2026-05-14 20:44:55 -0500

    add a linter

--- app/core/calculations.py
+++ app/core/calculations.py
@@ -1,44 +1,35 @@
 from typing import Any
 from app.schemas.models import ItemizedReciept, SplitBreakdown
 
-def split_calculator(itemized_reciept: ItemizedReciept, split_breakdown: SplitBreakdown) -> dict: 
+
+def split_calculator(
+    itemized_reciept: ItemizedReciept, split_breakdown: SplitBreakdown
+) -> dict:
     """
-    meat of the splitting algorithm 
+    meat of the splitting algorithm
 
-    takes in an itemized reciept, who got what and then returns the per person split 
+    takes in an itemized reciept, who got what and then returns the per person split
 
     """
 
-    reciept_dict = {} 
-    people_dict = {} 
+    reciept_dict = {}
+    people_dict = {}
 
     ## building the reciept dictionary
-    for item in itemized_reciept.receipt: 
+    for item in itemized_reciept.receipt:
         reciept_dict[item.name] = item.price
 
-    ## building the people dictionary 
-    for shared_item in split_breakdown.items: 
-        for person in shared_item.people: 
+    ## building the people dictionary
+    for shared_item in split_breakdown.items:
+        for person in shared_item.people:
             people_dict[person] = 0
 
-    ## meat of the problem 
-    for shared_item in split_breakdown.items: 
-        if shared_item.item in reciept_dict: 
+    ## meat of the problem
+    for shared_item in split_breakdown.items:
+        if shared_item.item in reciept_dict:
             cost_per_person = reciept_dict[shared_item.item] / len(shared_item.people)
 
-            for person in shared_item.people: 
+            for person in shared_item.people:
                 people_dict[person] += cost_per_person
 
     return people_dict
-    
-
-    
-
-        
-        
-        
-            
-
-    
-
-    
